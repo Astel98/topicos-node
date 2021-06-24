33af7aec8b7ce111676c8c412f3ab7b8735c33a5
Hugo Lora <hugolora2014@gmail.com>
2021-06-24 10:18:38 -0400

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middlewares();
+
+
+        //Rutas de mi aplicacion
+        this.routes();
+    }
+
+    middlewares() {
+
+        //CORS
+        this.app.use(cors());
+
+        //Lectura y parse del body
+
+        this.app.use(express.json());
+
+        //Carpeta Publica
+        this.app.use(express.static('public'));
+
+
+    }
+
+    routes() {
+
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor Corriendo en el puerto', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
